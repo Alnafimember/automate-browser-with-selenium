f5c9f4c2cf362f8b255b764ec83681819b83c5aa
Muhammad Rafay <122479112+Alnafimember@users.noreply.github.com>
2023-05-10 17:08:25 +0500

    demowebite.py

--- demowebite.py
+++ demowebite.py
@@ -8,19 +8,19 @@ driver=webdriver.Firefox(executable_path=GeckoDriverManager().install())
 driver.get('https://www.saucedemo.com/')
 driver.maximize_window()
 time.sleep(5)
-# #This is for user-name
-# driver.find_element(By.ID,"user-name").send_keys("standard_user")
-# time.sleep(5)
-# #This is for password
-#
-# driver.find_element(By.ID,"password").send_keys("secret_sauce")
-# time.sleep(5)
-#
-# #login
-#
-# driver.find_element(By.ID,"login-button").click()
-#
-#
+#This is for user-name
+driver.find_element(By.ID,"user-name").send_keys("standard_user")
+time.sleep(5)
+#This is for password
+
+driver.find_element(By.ID,"password").send_keys("secret_sauce")
+time.sleep(5)
+
+#login
+
+driver.find_element(By.ID,"login-button").click()
+
+
 
 # time.sleep(40)
 
